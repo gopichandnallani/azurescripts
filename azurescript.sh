f5dc22d587c38960c5437d1d43bfa21da8542ff9
Gopichand <nallanigopichand@gmail.com>
2022-08-05 21:16:18 -0700

    parameterised the script to deploy base infra automatically

--- azurescript.sh
+++ azurescript.sh
@@ -10,14 +10,13 @@ echo "creating resource group $RG"
 az group create -l ${zone} -n ${RG}
 
 echo "creating VNET $vnet"
-az network vnet create -n ${vnet} -g development-l eastus --address-prefix 10.1.0.0/16 \
+az network vnet create -n ${vnet} -g ${RG} --address-prefix 10.1.0.0/16 \
 --subnet-name ${subnet}-1 --subnet-prefix 10.1.1.0/24 
 
 echo "subnet cretaing....."
-az network vnet subnet create --address-prefixes 10.1.1.0/24 -n production-subnet-1 -g development --vnet-name production
-az network vnet subnet create --address-prefixes 10.1.2.0/24 -n production-subnet-2 -g development --vnet-name production
-az network vnet subnet create --address-prefixes 10.1.3.0/24 -n production-subnet-3 -g development --vnet-name production
-
+az network vnet subnet create --address-prefixes 10.1.1.0/24 -n ${subnet}-1 -g ${RG} --vnet-name ${vnet}
+az network vnet subnet create --address-prefixes 10.1.2.0/24 -n ${subnet}-2 -g ${RG} --vnet-name ${vnet}
+az network vnet subnet create --address-prefixes 10.1.3.0/24 -n ${subnet}-3 -g ${RG} --vnet-name ${vnet}
 echo "NSG creating...."
 az network nsg create --name ${nsgname} -g ${RG} --location ${zone} 
 
@@ -27,5 +26,5 @@ az network nsg rule create --name ${nsgrule} --nsg-name ${nsgname} -g ${RG} --pr
 --protocol Tcp --access allow --description "allowing all the traffic"
 
 echo "VM creating....s"
-az vm create --name produvtion-vm --location eastus --image UbuntuLTS --vnet-name production --subnet production-subnet-1 --public-ip-sku Standard \
--g development --admin-username gopichand --admin-password "world@123456" --size Standard_B1s --nsg development-eastus-nsg
+az vm create --name ${RG}-vm --location ${zone} --image UbuntuLTS --vnet-name ${vnet} --subnet ${subnet}-1 --public-ip-sku Standard \
+-g ${RG} --admin-username gopichand --admin-password "world@123456" --size Standard_B1s --nsg ${nsgname}
